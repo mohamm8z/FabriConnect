10a694503791bad7f8d2afe006875d3a921a5cc8
keta <156949721+ketajani@users.noreply.github.com>
2024-08-05 14:14:23 -0400

    frontend for retailer products page

---
+++ linux/category.dart
@@ -0,0 +1,16 @@
+const categoryList=["Mens","Kids","Womens"];
+const categoryListMen=["Jeans","Shirt","T-shirt","Jacket","Sportwear","Suit"];
+const categoryimagesMen=["assets/images/jeans.png","assets/images/shirt.png","assets/images/tshirt.png","assets/images/jacket.png","assets/images/sports.png","assets/images/suit.png"];
+
+const categoryListKids=["Baby jeans","Baby t-shirt","Baby Shots","Baby Jacket","Baby overall","Baby suit"];
+const categoryimagesKids=["assets/images/babyjeans.jpg","assets/images/babytshirt.jpg","assets/images/babyshort.jpg","assets/images/babyjacket.jpg","assets/images/babyoverall.jpg","assets/images/babysuit.jpg"];
+
+const categoryListWomens=["Dress","jeans","Tshirt","Saree","Sportwear","Skirt"];
+const categoryimagesWomens=["assets/images/dress.jpg","assets/images/jeans1.jpg","assets/images/tshirt1.jpg","assets/images/saree.jpg","assets/images/sport.jpg","assets/images/skirt.jpg"];
+
+const itemdetailsbuttontext=["Video","Review","Privacypolicy","ReturnPolicy","SupportPolicy"];
+const profiledetails=["my orders","my wishlist","my messages"];
+const profileimages=["assets/images/meatball.png","assets/images/meatball.png","assets/images/message.jpg",];
+
+const paymentMethodsList=["Cash on delivery","Google Pay","Debit/Credit card"];
+const paymentMethodsimageList = ["assets/images/cod.jpg","assets/images/gpay.jpg","assets/images/card.jpg",];
